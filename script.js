c524df3aab5f9cd1fdb8d2ca9d4f9743bc5dd5a4
Siddharth Maurya <145599412+MauryaSiddharth@users.noreply.github.com>
2023-09-26 22:28:07 +0530

    Add files via upload
    Basic Todo app

---
+++ script.js
@@ -0,0 +1,18 @@
+let input = document.getElementById("inp");
+let text = document.querySelector(".text")
+
+function ADD(){
+    if(input.value == ""){
+        alert("Please Enter The Task");
+    }else{
+        let newEle = document.createElement("ul");
+        newEle.innerHTML=`${input.value} <i class="fa-solid fa-trash"></i> `;
+        text.appendChild(newEle);
+        input.value="";
+        newEle.querySelector("i").addEventListener("click",remove);
+         function remove(){
+              newEle.remove()
+         }
+        
+    }
+}
